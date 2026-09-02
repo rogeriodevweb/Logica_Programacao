6c5654e71fec047b9cfb3ac56e1337775505d45f
rogeriodevweb <piuutrapp@gmail.com>
2026-09-02 17:53:21 -0300

    aprendemos sobre variaveis

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -0,0 +1,38 @@
+//Comentario de 1 linha
+
+/*Comentario
+de varias
+linhas*/
+
+package com.mycompany.variaveis;
+    
+/* @author RogerioSilva */
+
+public class Variaveis {
+    //A classe main e quem executa os codigos
+    public static void main(String[] args) {
+        /*codigos de saida
+        - São utilizados para imprimir/mostrar 
+        dados e mensagens.        
+        */
+        System.out.println("Ola");
+        System.out.println("eu sou um programador web");
+        //println pula linha, print mostra na frente do outro
+        System.out.print("eu sei programar");
+        System.out.println(" em Java ");
+        
+        //criacao de variaveis
+        
+       int idade = 22;
+       double altura = 1.85;
+       float peso = 70;
+       String nome = "rogerio";
+       boolean casado = false;
+       
+       System.out.println("Ola "+nome);
+       System.out.println("Voce tem "+idade+ " de idade"); 
+        
+        
+        
+    }   //Fim da main
+}
